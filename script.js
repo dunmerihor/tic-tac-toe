764f3ff0327feb655a19bad93f3b8d9fb5fc730c
dunmeri <ethanq95@gmail.com>
2022-09-06 00:35:58 +0700

    Add possibility to mark game board

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-const gameBoard = (function() {
-    let gameBoardInfo = ['x', 'o',  'o',  'x',  'o',  'x',  'o',  'x',  'x'];
+const Player = (sign, turn) => {
+    const makeMove = (isCell, cellID) => {
+        if (turn) {
+            gameBoard.clickOnBoard(sign, cellID);
+            turn = false;
+        }
+    }
+
+    const changeTurn = () => turn = turn ? false : true;
+
+    const getSign = () => {return sign};
+
+    const getTurn = () => {return turn};
+
+    return {
+        makeMove,
+        changeTurn,
+        getSign,
+        getTurn
+    }
+}
+
+const player1 = Player('x', true);
+const player2 = Player('o', false);
+
+const gameBoard = (function(player1, player2) {
+    let gameBoardInfo = ['', '',  '',  '',  '',  '',  '',  '',  ''];
     const gameWrapper = document.querySelector('div.game-wrapper');
     const cellList = document.querySelector('ul#cell-list');
 
@@ -11,13 +36,33 @@ const gameBoard = (function() {
             cell.classList.add('cell');
             cell.innerHTML = gameBoardInfo[i];
             cellList.appendChild(cell);
+            if (gameBoardInfo[i] === '') {
+                cell.addEventListener('click', putSign);
+            }
         }
     };
-    
-    const clickOnBoard = (element, cellID) => {
-        gameBoardInfo[cellID] = element;
+
+    const clickOnBoard = (sign, cellID) => {
+        gameBoardInfo[cellID] = sign;
         render();
     }
 
+    const putSign = (event) => {
+        if (player1.getTurn() === true) {
+            clickOnBoard(player1.getSign(), event.target.id);
+            player1.changeTurn();
+            player2.changeTurn();
+        }
+        else {
+            clickOnBoard(player2.getSign(), event.target.id);
+            player1.changeTurn();
+            player2.changeTurn();
+        }
+    }
+
     render();
-})()
+
+    return {
+        clickOnBoard,
+    };
+})(player1, player2);
